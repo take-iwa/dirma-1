b79ee7f855692f13e17ed776828a08d2b2735e4b
平原　俊幸 <t.hirahara21@gmail.com>
2017-08-31 08:45:30 +0900

    Commit

---
+++ php/company_dashboard.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'init.php';
+sessChk();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>DIRMA - </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link href="../css/style.css" rel="stylesheet">
+  </head>
+  <body>
+
+  </body>
+</html>
--- php/index.php
+++ php/index.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/index.css">
+    <link rel="shortcut icon" href="../img/dirma_favicon.ico">
     <style type="text/css">
         a {
             text-decoration: none;
@@ -14,12 +18,27 @@
 </head>
 
 <body>
-    <div id="body">
-        <div id="top">
-            <a href="index.php"><img src="../img/dirmalogo.png" alt="logo" width="100px" height="60px" id="dlogo"></a>
-            <p id="menu">ログイン</p>
-        </div>
+    <nav class="navbar navbar-fixed-top">
+      <div class="navbar-header">
+         <!-- logo img -->
+         <a class="navbar-brand" href=""></a>
+         <!-- toggle -->
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-nav">
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+         </button>
+      </div>
 
+      <div id="gnavi" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="signup.php">新規登録</a>
+          <li><a href="signin.php">ログイン</a>
+          <li><a href="#">スカウト</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div id="body">
         <div id="main">
             <p id="mes1">世界を変えていく企業との出会いを、あなたに。</p>
             <p id="mes2">求人が探せる、詳しくわかる、スカウトが届く。<br>大手グローバル企業の求人情報に特化した転職サイト「Dirma（ダーマ）」。</p>

652e2b533e132a83a274455bbf807291c781c132
ooiwa <h.ooiwa7701@gmail.com>
2017-10-12 09:13:25 +0900

    画像png→jpgに変更

--- index.php
+++ index.php
@@ -58,14 +58,14 @@
     <div class="jumbotron">
       <section class="container" style="margin-top:20px">
         <div class="col-md-12 text-center center-block">
-          <h1><img src="./img/dirma_white.png" alt="Dirma" width="300" height="auto"></h1>
+          <h1><img src="./img/dirma_white.png" alt="Dirma" class="img-responsive center-block"></h1>
           <p id="main">世界を変えていく企業との出会いを、<br>あなたに。</p>
         </div>
         <div class="row top-margin">
-          <div class="col-xs-6 col-md-push-3 col-md-3 text-center">
+          <div class="col-xs-6 col-sm-push-2 col-sm-4 col-md-push-3 col-md-3 text-center">
             <a href="signin.php" class="button_left">　ログイン / 会員登録</a>
           </div>
-          <div class="col-xs-6 col-md-push-3 col-md-3 text-center">
+          <div class="col-xs-6 col-sm-push-2 col-sm-4 col-md-push-3 col-md-3 text-center">
             <a href="search.php" class="button_right">　求人検索</a>
           </div>
         </div>
@@ -92,7 +92,7 @@
             </p>
           </div>
           <div class="col-md-6 col-xs-12 center-block">
-            <img src="./img/future1.png" class="img-responsive">
+            <img src="./img/future1.jpg" class="img-responsive">
           </div>
         </div>
       </div>
@@ -112,7 +112,7 @@
           </p>
         </div>
         <div class="col-md-6 col-md-pull-6 col-xs-12 center-block">
-          <img src="./img/future2.png" class="img-responsive">
+          <img src="./img/future2.jpg" class="img-responsive">
         </div>
       </div>
     </section>
@@ -132,7 +132,7 @@
             </p>
           </div>
           <div class="col-md-6 col-xs-12 center-block">
-            <img src="./img/future3.png" class="img-responsive">
+            <img src="./img/future3.jpg" class="img-responsive">
           </div>
         </div>
       </div>
@@ -154,7 +154,7 @@
     <section class="container">
       <div class="row">
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="http://www.recruit.jp/employment/" target="_blank"><img src="./img/1.png" width="380" height="auto">
+          <a href="http://www.recruit.jp/employment/" target="_blank"><img src="./img/1.jpg" class="img-responsive">
             <p>株式会社リクルートホールディングス</p>
           </a>
           <p class="#1">#人材 #海外 #他事業</p>
@@ -163,14 +163,14 @@
         </div>
 
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="https://corp.rakuten.co.jp/careers/top/" target="_blank"><img src="./img/2.png" width="380" height="auto">
+          <a href="https://corp.rakuten.co.jp/careers/top/" target="_blank"><img src="./img/2.jpg" class="img-responsive">
             <p>楽天株式会社</p>
           </a>
           <p class="#1">#Eコマース #英語 #海外</p>
           <hr>
         </div>
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="https://www.fastretailing.com/employment/ja/" target="_blank"><img src="./img/3.png" width="380" height="auto">
+          <a href="https://www.fastretailing.com/employment/ja/" target="_blank"><img src="./img/3.jpg" class="img-responsive">
             <p>ファーストリテイリング</p>
           </a>
           <p class="#1">#ファッション #7時出社 #新規事業</p>
@@ -179,21 +179,21 @@
       </div>
       <div class="row">
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="http://mitsui-career.com/" target="_blank"><img src="./img/4.png" width="380" height="auto">
+          <a href="http://mitsui-career.com/" target="_blank"><img src="./img/4.jpg" class="img-responsive">
             <p>三井物産株式会社</p>
           </a>
           <p class="#1">#海外 #他事業 #個性的</p>
           <hr>
         </div>
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="http://www-07.ibm.com/employment/jp/cp01/" target="_blank"><img src="./img/5.png" width="380" height="auto">
+          <a href="http://www-07.ibm.com/employment/jp/cp01/" target="_blank"><img src="./img/5.jpg" class="img-responsive">
             <p>株式会社日本IBM</p>
           </a>
           <p class="#1">#次世代ビジネス #コグニティブ #システム</p>
           <hr>
         </div>
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="https://www.amazon.co.jp/b/ref=s9_acss_bw_h1_hrmid_md1_w?node=5121262051&pf_rd_m=A3P5ROKL5A1OLE&pf_rd_s=merchandised-search-1&pf_rd_r=SMC1W6ZEVE1G43SZ030N&pf_rd_t=101&pf_rd_p=6b72eb49-8139-43e1-ba7f-3a5db6dd7975&pf_rd_i=5121604051"><img src="./img/6.png" target="_blank" width="380" height="auto">
+          <a href="https://www.amazon.co.jp/b/ref=s9_acss_bw_h1_hrmid_md1_w?node=5121262051&pf_rd_m=A3P5ROKL5A1OLE&pf_rd_s=merchandised-search-1&pf_rd_r=SMC1W6ZEVE1G43SZ030N&pf_rd_t=101&pf_rd_p=6b72eb49-8139-43e1-ba7f-3a5db6dd7975&pf_rd_i=5121604051"><img src="./img/6.jpg" target="_blank" class="img-responsive">
             <p>株式会社アマゾン</p>
           </a>
           <p class="#1">#EC #革新的 #Kindle</p>
@@ -202,21 +202,21 @@
       </div>
       <div class="row">
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="http://www.toyota-careers.com/automated_driving/?adid=ag454_lis_google_src._.txt_engineer_normal2.pc.0178.toyotacareers.170720.GG01..&padid=ag454_lis_google_src._.txt_engineer_normal2.pc.0178.toyotacareers.170720.GG01.." target="_blank"><img src="./img/7.png" width="380" height="auto">
+          <a href="http://www.toyota-careers.com/automated_driving/?adid=ag454_lis_google_src._.txt_engineer_normal2.pc.0178.toyotacareers.170720.GG01..&padid=ag454_lis_google_src._.txt_engineer_normal2.pc.0178.toyotacareers.170720.GG01.." target="_blank"><img src="./img/7.jpg" class="img-responsive">
             <p>トヨタ自動車株式会社</p>
           </a>
           <p class="#1">#自動車 #北米 #海外</p>
           <hr>
         </div>
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="http://www.nomuraholdings.com/jp/careers/" target="_blank"><img src="./img/8.png" width="380" height="auto">
+          <a href="http://www.nomuraholdings.com/jp/careers/" target="_blank"><img src="./img/8.jpg" class="img-responsive">
             <p>野村証券株式会社</p>
           </a>
           <p class="#1">#証券 #リテール #人</p>
           <hr>
         </div>
         <div class="col-xs-12 col-md-4 center-block">
-          <a href="http://recruit.softbank.jp/career/"><img src="./img/9.png" target="_blank" width="380" height="auto">
+          <a href="http://recruit.softbank.jp/career/"><img src="./img/9.jpg" target="_blank" class="img-responsive">
             <p>ソフトバンクグループ</p>
           </a>
           <p class="#1">#通信 #携帯 #インフラ</p>
@@ -228,7 +228,7 @@
 
   <footer class="footer">
     <section class="container">
-      <div class="col-md-offset-6 col-md-6 text-center">
+      <div class="col-md-offset-4 col-md-8 col-xs-12 text-center">
         <ul class="nav navbar-nav">
           <li><a href="#about">運営会社</a></li>
           <li><a href="#about">利用規約</a></li>
@@ -236,7 +236,7 @@
           <li><a href="#contact">お問い合わせ</a></li>
         </ul>
       </div>
-      <div class="col-md-12 text-center">
+      <div class="col-md-12 col-xs-12 text-center">
         <small>Copyright Dirma, Inc.</small>
       </div>
     </section>
